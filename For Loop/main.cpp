a0bb2af959c7dbb352f7e8aeb15b425782a60290
Muaz Bin Gulfam <106175817+Dangeryego@users.noreply.github.com>
2022-08-16 01:55:18 +0500

    Providing Even numbers up till 100

---
+++ For Loop/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i{1};
+    for (i; i <= 100; ++i) {
+       if (i % 2 == 0){
+            cout << i;
+        }else
+        {
+            cout << "\n";
+        }
+    }
+    return 0;
+}
